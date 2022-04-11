f9277e22a8959a3e8a309993ebf69dd7fbe9fb99
janinanarhi <janinanarhi92@gmail.com>
2022-04-11 23:18:27 +0300

    siivousta

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+document
+  .getElementById("syotaNimi")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var txt;
+    var person = document.getElementById("name").value;
+
+    if (person == null || person == "") {
+      txt = "Kirjoitithan kenttään nimesi?";
+    } else {
+      txt = "Hei " + person + '! Nimeni on Janina. Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="kuvat/smiley.png" alt="hymiöpekut"/>';
+    }
+
+    document.getElementById("tervehdys").innerHTML = txt;
+  });
