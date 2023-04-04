764cb79b9a257a4463841dd74ebe9e532ce22a79
Janina Närhi <janinanarhi92@gmail.com>
2023-04-04 18:38:13 +0300

    add views

--- pianokoulu/aloitussivu/aloitussivu.js
+++ pianokoulu/aloitussivu/aloitussivu.js
@@ -2,14 +2,14 @@ document
   .getElementById("syotaNimi")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
     var txt;
     var person = document.getElementById("name").value;
-
+    
+    console.log('HELLOUTA', person)
     if (person == null || person == "") {
       txt = "Kirjoitithan kenttään nimesi?";
     } else {
-      txt = "Hei " + person + '! Nimeni on Janina. Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="../kuvat/smiley.png" alt="hymiöpeukut"/>';
+      txt = `Hei ${person}! Nimeni on Janina. Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="kuvat_toiminta/smiley.png" alt="hymiöpeukut"/>`;
     }
 
     document.getElementById("tervehdys").innerHTML = txt;
