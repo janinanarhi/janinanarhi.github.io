8f505614c967ee00f26a44539801c7489094ca37
Janina Närhi <janinanarhi92@gmail.com>
2023-04-26 21:29:32 +0300

    add new stuff

--- pianokoulu/aloitussivu/aloitussivu.js
+++ pianokoulu/aloitussivu/aloitussivu.js
@@ -1,16 +1,30 @@
 document
-  .getElementById("syotaNimi")
+  .getElementById("syotaTiedot")
   .addEventListener("submit", function (event) {
     event.preventDefault();
     var txt;
     var person = document.getElementById("name").value;
+    var ika = document.getElementById("age").value;
     
-    console.log('HELLOUTA', person)
-    if (person == null || person == "") {
-      txt = "Kirjoitithan kenttään nimesi?";
+    
+    /*if ((person == null || person == "") || (ika == null || ika == "")) {
+      txt = "Kirjoitithan kenttiin nimesi ja ikäsi?";
     } else {
-      txt = `Hei ${person}! Nimeni on Janina. Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="kuvat_toiminta/smiley.png" alt="hymiöpeukut"/>`;
+      txt = `Hei ${person}! Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="kuvat_toiminta/smiley.png" alt="hymiöpeukut" width="130px" height="100px" />`;
+    }
+
+    document.getElementById("tervehdys").innerHTML = txt;*/
+
+    if (person == null || person == "")  {
+      txt = "Kirjoitithan kenttään nimesi?";
+    } else if (!isNaN(person)) {
+      txt = "Numerot eivät valitettavasti nimessä käy."
+    } else if (isNaN(ika)) {
+        txt = "Kirjoitithan kenttään ikäsi?";
+    } else {
+      txt = `Hei ${person}! Hauska tutustua!<br>Aloitetaanko?</br><br><img class="hymiot" src="kuvat_toiminta/smiley.png" alt="hymiöpeukut" width="130px" height="100px" />`;
     }
 
     document.getElementById("tervehdys").innerHTML = txt;
   });
+  
